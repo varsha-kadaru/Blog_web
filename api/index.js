646341ea63b91f63b70ea0fedb11b01e730394f1
varsha-kadaru <107875699+varsha-kadaru@users.noreply.github.com>
2023-07-01 10:58:31 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,42 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+const authRoute=require("./routes/auth")
+const userRoute=require("./routes/users")
+const postRoute=require("./routes/posts")
+const categoryRoute=require("./routes/categories")
+const multer=require("multer")
+
+
+dotenv.config()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(console.log("Connection succesful"))
+.catch(err=>console.log(err))
+
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")
+    },filename:(req,file,cb)=>{
+        cb(null,"hello.png");
+    }
+})
+
+const upload=multer({storage:storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("file has been uploaded")
+})
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/posts",postRoute);
+app.use("/api/categories",categoryRoute);
+
+app.listen("3000",()=>{
+    console.log("hell backened");
+})
---
+++ api/routes/auth.js
@@ -0,0 +1,43 @@
+const router=require("express").Router();
+const User=require("../models/User")
+const bcrypt=require("bcrypt");
+//register
+router.post("/register",async(req,res)=>{
+    try{
+        const salt=await bcrypt.genSalt(10)
+        const hashpassword=await bcrypt.hash(req.body.password,salt);
+        const newUser=new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashpassword
+        });
+        const user=await newUser.save();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+//login
+router.post("/login",async(req,res)=>{
+    try{
+        const user=await User.findOne({
+            username:req.body.username
+        })
+        if(!user) 
+            return res.status(400).json("Wrong credentials");
+        const validate=await bcrypt.compare(req.body.password,user.password)
+        if(!validate) 
+            return res.status(400).json("Wrong credentials");
+
+        const {password,...others}=user._doc //excepts password all are sent as res
+
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports=router
